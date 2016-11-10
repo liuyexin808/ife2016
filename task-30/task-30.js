4053028e104d52831b510379470dee628c6f9ed6
liuyexin808 <liuyexin808@gmail.com>
2016-11-10 20:02:39 +0800

    fix bug

--- task-30/task-30.js
+++ task-30/task-30.js
@@ -1,47 +1,49 @@
+(function() {
+
 var arr = [
 	["必填,名称长度为4-16个字符","名称格式正确","名称格式错误"],
 	["必填,密码长度6～14位字符","密码格式正确","密码格式错误"],
 	["请重复输入密码","密码输入一致","密码输入不一致"],
 	["必填,example@example.com","邮箱格式正确","邮箱格式错误"],
 	["必填,请输入11位手机号码","手机格式正确","手机格式错误"]
-]	
+],	
 	content = $$("input"),
 	info = $$(".info");
 
 function validate(i) {
 	var val = content[i].value.trim(),
 		flag;
-		if(!val) {
-			validateEmpty(i);
-			return;
-		}
-		switch(i) {
-			case 0: if(countLength(val) >= 4 && countLength(val) <= 16 ) {
-					flag =  true;
-				}	
-				break;
-			case 1: if(countLength(val) >= 6 && countLength(val) <= 14) {
-					flag =  true;
-				}	
-				break;
-			case 2: if(val == content[1].value) {
-					flag =  true;
-				}	
-				break;
-			case 3: if(/^[a-z_0-9.-]{1,64}@([a-z0-9-]{1,200}.){1,5}[a-z]{1,6}$/i.test(val)) {
-					flag =  true;
-				}
-				break;
-			case 4: if(/^1\d{10}$/.test(val)) {
-					flag =  true;
-				}
-				break;
-		}
-		if(flag) {
-			validateCorrect(i);
-		} else {
-			validateIncorrect(i);
-		}
+	if(!val) {
+		validateEmpty(i);
+		return;
+	}
+	switch(i) {
+		case 0: if(countLength(val) >= 4 && countLength(val) <= 16 ) {
+				flag =  true;
+			}	
+			break;
+		case 1: if(countLength(val) >= 6 && countLength(val) <= 14) {
+				flag =  true;
+			}	
+			break;
+		case 2: if(val == content[1].value) {
+				flag =  true;
+			}	
+			break;
+		case 3: if(/^[a-z_0-9.-]{1,64}@([a-z0-9-]{1,200}.){1,5}[a-z]{1,6}$/i.test(val)) {
+				flag =  true;
+			}
+			break;
+		case 4: if(/^1\d{10}$/.test(val)) {
+				flag =  true;
+			}
+			break;
+	}
+	if(flag) {
+		validateCorrect(i);
+	} else {
+		validateIncorrect(i);
+	}
 }
 
 function countLength(str) {
@@ -117,8 +119,12 @@ function sumbit() {
 	}
 }
 
-(function init() {
+function init() {
 	focusHandler();
 	blurHandlder();
 	addEvent($("button"),"click",sumbit);
+}
+
+init()
+
 })()
